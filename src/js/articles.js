c15e247959fc72c6880e4a92dacd0b0bbb1ce6ce
Andrew Leith <andrew.leith@cds-snc.ca>
2023-12-11 14:21:22 -0400

    chore(formatting): apply prettier to all source files

--- src/js/articles.js
+++ src/js/articles.js
@@ -1,40 +1,43 @@
 /**
  * This module takes care of loading the incident history page from GC Articles.
- * 
+ *
  * It exposes one function at the global scope:
  * - LoadIncidentHistory() - loads the incident history content
- * 
+ *
  * @todo: Add caching
  */
 (async function () {
-    const incident_history_pages = {
-      en: "https://articles.alpha.canada.ca/notification-gc-notify/wp-json/wp/v2/pages?slug=system-status&lang=en",
-      fr: "https://articles.alpha.canada.ca/notification-gc-notify/wp-json/wp/v2/pages?slug=etat-du-systeme&lang=fr",
-    }
+  const incident_history_pages = {
+    en: "https://articles.alpha.canada.ca/notification-gc-notify/wp-json/wp/v2/pages?slug=system-status&lang=en",
+    fr: "https://articles.alpha.canada.ca/notification-gc-notify/wp-json/wp/v2/pages?slug=etat-du-systeme&lang=fr",
+  };
 
-    /**
-     * Get the incident history page from GC Articles and display it
-     * @param {string} url - The URL of the incident history page
-     * @param {string} loaderId - The ID of the loader element
-     * @param {string} contentId - The ID of the content element
-     * 
-     * @returns {void}
-     */
-    window.LoadIncidentHistory = async function({ lang, loaderId, contentId }) {
-      document.getElementById(contentId).innerHTML = "";
-      document.getElementById(loaderId).hidden = false;
+  /**
+   * Get the incident history page from GC Articles and display it
+   * @param {string} url - The URL of the incident history page
+   * @param {string} loaderId - The ID of the loader element
+   * @param {string} contentId - The ID of the content element
+   *
+   * @returns {void}
+   */
+  window.LoadIncidentHistory = async function ({ lang, loaderId, contentId }) {
+    document.getElementById(contentId).innerHTML = "";
+    document.getElementById(loaderId).hidden = false;
 
-      const response = await fetch(incident_history_pages[lang]);
-      const incidents = await response.json();
-      incident_html = incidents[0].content.rendered;
+    const response = await fetch(incident_history_pages[lang]);
+    const incidents = await response.json();
+    incident_html = incidents[0].content.rendered;
 
-      // fix up h3's to use tailwind
-      incident_html = incident_html.replace(/wp-block-heading/g, 'text-3xl text-gray-800 dark:text-gray-400 font-bold leading-none mb-3 mt-10');
+    // fix up h3's to use tailwind
+    incident_html = incident_html.replace(
+      /wp-block-heading/g,
+      "text-3xl text-gray-800 dark:text-gray-400 font-bold leading-none mb-3 mt-10",
+    );
 
-      // remove h2
-      incident_html = incident_html.replace(/<h2.*\/h2>/g, '');
+    // remove h2
+    incident_html = incident_html.replace(/<h2.*\/h2>/g, "");
 
-      document.getElementById(loaderId).hidden = true;
-      document.getElementById(contentId).innerHTML = incident_html;
-    }
-  })();
+    document.getElementById(loaderId).hidden = true;
+    document.getElementById(contentId).innerHTML = incident_html;
+  };
+})();
--- src/js/component_status.js
+++ src/js/component_status.js
@@ -3,47 +3,48 @@
  * from the response.json file.
  */
 (async function () {
-  const STATUS_DATA_URL = 'https://raw.githubusercontent.com/cds-snc/notification-system-status-frontend/main/response.json';
+  const STATUS_DATA_URL =
+    "https://raw.githubusercontent.com/cds-snc/notification-system-status-frontend/main/response.json";
   const COMPONENTS = {
-    'api': {
-      id: 'api',
-      icon: 'code'
-    }, 
-    'admin': {
-      id: 'admin',
-      icon: 'desktop'
-    }, 
-    'email': {
-      id: 'email',
-      icon: 'envelope'
+    api: {
+      id: "api",
+      icon: "code",
     },
-    'sms': {
-      id: 'sms',
-      icon: 'comment'
-    }
+    admin: {
+      id: "admin",
+      icon: "desktop",
+    },
+    email: {
+      id: "email",
+      icon: "envelope",
+    },
+    sms: {
+      id: "sms",
+      icon: "comment",
+    },
   };
   const COMPONENT_STATES = {
-    'up': {
+    up: {
       text: '<span data-i18n="status_up">Up</span>',
-      icon: 'fa-regular fa-circle-check',
-      color: 'text-green-700 dark:text-green-300'
+      icon: "fa-regular fa-circle-check",
+      color: "text-green-700 dark:text-green-300",
     },
-    'down': {
+    down: {
       text: '<span data-i18n="status_down">Down</span>',
-      icon: 'fa-solid fa-circle-exclamation',
-      color: 'text-red-600 dark:text-red-400'
+      icon: "fa-solid fa-circle-exclamation",
+      color: "text-red-600 dark:text-red-400",
     },
-    'degraded': {
+    degraded: {
       text: '<span data-i18n="status_degraded">Degraded</span>',
-      icon: 'fa-solid fa-triangle-exclamation',
-      color: 'text-yellow-700 dark:text-yellow-500'
+      icon: "fa-solid fa-triangle-exclamation",
+      color: "text-yellow-700 dark:text-yellow-500",
     },
-    'unknown': {
+    unknown: {
       text: '<span data-i18n="status_loading">Loading...</span>',
-      icon: 'fa-solid fa-spinner fa-spin',
-      color: 'text-neutral-500'
-    }
-  }
+      icon: "fa-solid fa-spinner fa-spin",
+      color: "text-neutral-500",
+    },
+  };
   const COMPONENT_HTML = ({ component_id, component_status }) => `
     <li class="py-3 sm:py-4 outline-offset-6" tabindex="0" id="${component_id}">
       <div class="flex items-start">
@@ -65,31 +66,33 @@
   // Render the initial component status list
   for (const key in COMPONENTS) {
     const component = COMPONENTS[key];
-    document.getElementById('component_status_list').innerHTML += COMPONENT_HTML({
-      component_id: component.id,
-      component_status: COMPONENT_STATES['unknown'],
-    });
+    document.getElementById("component_status_list").innerHTML +=
+      COMPONENT_HTML({
+        component_id: component.id,
+        component_status: COMPONENT_STATES["unknown"],
+      });
   }
 
   /**
    * Loads the component status information from the response.json file
    * @param {Function} i18n_loader  - Callback function to re-run i18n after statuses are loaded
    */
-  window.LoadComponentStatus = async function(i18n_loader) {
+  window.LoadComponentStatus = async function (i18n_loader) {
     const response = await fetch(STATUS_DATA_URL);
     const component_statuses = await response.json();
-    
-    document.getElementById('component_status_list').innerHTML = "";
+
+    document.getElementById("component_status_list").innerHTML = "";
     for (const key in component_statuses) {
       const component = component_statuses[key];
-      document.getElementById('component_status_list').innerHTML += COMPONENT_HTML({
-        component_id: component.id,
-        component_status: COMPONENT_STATES[component.status],
-      });
+      document.getElementById("component_status_list").innerHTML +=
+        COMPONENT_HTML({
+          component_id: component.id,
+          component_status: COMPONENT_STATES[component.status],
+        });
     }
 
     if (i18n_loader) {
       i18n_loader();
     }
-  }
+  };
 })();
